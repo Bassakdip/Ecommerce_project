b09c543297bc319f57076199b3c712e7ec05fda5
Dipankar Basak <143477069+Bassakdip@users.noreply.github.com>
2024-04-20 10:24:05 +0530

    Update Hader.js

--- src/Components/Layout/Hader.js
+++ src/Components/Layout/Hader.js
@@ -1,23 +1,43 @@
-import React, { Fragment } from 'react'
-import classess from './Hader.module.css';
+import React, {useContext} from 'react'
+import classess from './Header.module.css';
+import { Dropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart } from "react-icons/fa";
+import Cart from '../cart/Cart';
+import CartContext from '../../Context/CartContext';
 
-const Hader = () => {
+
+const Header = () => {
+    const cartCtx = useContext(CartContext);
+  const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.quantity;
+  }, 0);
     return (
-        <Fragment>
+        <div>
             <header className={classess.header}>
                 <ul className={classess.ul}>
-                    <li className={classess.li}>Home</li>
-                    <li className={classess.li}>Store</li>
-                    <li className={classess.li}>About Us</li>
-                    
-                    <button className={classess.cart}> Cart
-                        <span className={classess.cart_number}>0</span>
-                    </button>
+                    <li className={classess.li} to={"/home"}>Home</li>
+                    <li className={classess.li} to={"/"}>Store</li>
+                    <li className={classess.li} to={"/about"}>About Us</li>
+                    <li className={classess.li} to={"/contact"}>Contact</li>
+                    <div className={classess.cart}>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success">
+                                <FaShoppingCart
+                                    color="white"
+                                    fontSize={"25px"}
+                                ></FaShoppingCart>
+                                <Badge color="white">{numberOfCartItem}</Badge>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu style={{ minWidth: 400 }}>
+                                <Cart></Cart>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                 </ul>
                 <h1>The Genesis</h1>
             </header>
-        </Fragment>
+        </div>
     )
 }
 
-export default Hader
+export default Header
